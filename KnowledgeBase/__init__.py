dea246bb9e3ab6093f450b19c8c333167dbad4b4
JIANG YUTONG <24066612g@connect.polyu.hk>
2025-04-14 16:00:22 +0800

    Update __init__.py

--- KnowledgeBase/__init__.py
+++ KnowledgeBase/__init__.py
@@ -1,6 +1,6 @@
 # 从同目录的模块导入并暴露在包级别
-from .Keyword_Matching import (
-    search_keyword_from_documents,
+from .BM25_method import (
+    bm25_keyword_search,
 )
 
 from .Vector_Space_Model import (
@@ -9,6 +9,11 @@ from .Vector_Space_Model import (
     FastText
 )
 
+from .TF_IDF_Keyword_Match import (
+    tfidf_keyword_search
+)
+
 __all__ = [
-    'search_keyword_from_documents',
+    'bm25_keyword_search',
+    'tfidf_keyword_search'
 ]
